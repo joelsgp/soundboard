b8be44c4f4ea6e5b4226ff0190175c595ec7e203
jmcb <joelsgp@protonmail.com>
2023-05-11 15:50:07 +0100

    Indexer improvements

--- indexer.py
+++ indexer.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 INDEX_NAME = "index.json"
-RE_YT_ID = re.compile(r"^(.*)-([a-zA-Z0-9]{11}).*$")
+RE_YT_ID = re.compile(r"^(.*)-([\w\-]{11}).*$")
 
 Index = dict[str, str]
 
@@ -27,16 +27,18 @@ def index_file(index: Index, filepath: Path):
 
 
 def index_directory(directory: Path):
-    print(directory)
+    print(f"Entering '{directory}'")
 
     index = {}
+    files_indexed = 0
     for p in directory.iterdir():
         if p.is_dir():
             index_directory(p)
         elif p.is_file():
             index_file(index, p)
+            files_indexed += 1
 
-    print(f"'{directory}': {len(index)}")
+    print(f"'{directory}': {len(index)}/{files_indexed}")
 
     write_index(index, directory)
 
@@ -44,7 +46,8 @@ def index_directory(directory: Path):
 def write_index(index: Index, directory: Path):
     index_path = directory.joinpath(INDEX_NAME)
     with open(index_path, "w") as fp:
-        json.dump(index, fp)
+        json.dump(index, fp, indent=4)
+        fp.write('\n')
 
 
 def main():
